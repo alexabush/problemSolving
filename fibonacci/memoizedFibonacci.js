84127d263f1881b0e2face880be2116956e82358
Alex Bush <alexabush7@gmail.com>
2018-04-30 18:16:55 -0700

    starting memoizedFibonacci

---
+++ fibonacci/memoizedFibonacci.js
@@ -0,0 +1,14 @@
+function fibonacci(n, currentVal = n) {
+  if (n < 2) return n;
+  return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+const nums = Array.from({ length: 20 }, (num, index) => {
+  return index;
+});
+console.log('nums: ', nums);
+nums.map(runFib);
+
+function runFib(endNumber) {
+  console.log(fibonacci(endNumber));
+}
