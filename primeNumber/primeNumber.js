0ef043325c2e5b764415474a389d313fe6aa91bf
Alex Bush <alexabush7@gmail.com>
2018-05-07 18:29:28 -0700

    prime number boolean calc

---
+++ primeNumber/primeNumber.js
@@ -0,0 +1,13 @@
+function isPrime(num) {
+  for (let index = 2; index <= Math.sqrt(num); index++) {
+    if (num % index === 0) {
+      console.log('Not prime');
+      return;
+    }
+    console.log('Prime');
+  }
+}
+
+console.log(isPrime(5));
+console.log(isPrime(7));
+console.log(isPrime(12));
