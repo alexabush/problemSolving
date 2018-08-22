a05ebe79f81d7dc0772423e378b0930646c6d766
Alex Bush <alexabush7@gmail.com>
2018-08-22 11:41:26 -0700

    improved runtime to log n * n

--- interviewCake/arraysAndStrings/mergeMeetingTimes/mergeMeetingTimes.js
+++ interviewCake/arraysAndStrings/mergeMeetingTimes/mergeMeetingTimes.js
@@ -2,25 +2,24 @@ function mergeRanges(arr) {
   let sortedTimes = arr.sort((meeting1, meeting2) => {
     return meeting1.startTime > meeting2.startTime;
   });
-  let busyTimes = [];
-  for (let time of sortedTimes) {
+  let busyTimes = [sortedTimes[0]];
+  for (let i = 1; i < sortedTimes.length; i++) {
     let isAdded = false;
-    for (let busyTime of busyTimes) {
-      if (
-        time.startTime > busyTime.startTime &&
-        time.endTime < busyTime.endTime
-      ) {
-        isAdded = true;
-      } else if (
-        time.startTime > busyTime.startTime &&
-        time.startTime <= busyTime.endTime
-      ) {
-        busyTime.endTime = time.endTime;
-        isAdded = true;
-      }
+    let busyTime = busyTimes[busyTimes.length - 1];
+    if (
+      sortedTimes[i].startTime > busyTime.startTime &&
+      sortedTimes[i].endTime < busyTime.endTime
+    ) {
+      isAdded = true;
+    } else if (
+      sortedTimes[i].startTime > busyTime.startTime &&
+      sortedTimes[i].startTime <= busyTime.endTime
+    ) {
+      busyTime.endTime = sortedTimes[i].endTime;
+      isAdded = true;
     }
     if (!isAdded) {
-      busyTimes.push(time);
+      busyTimes.push(sortedTimes[i]);
     }
   }
   return busyTimes;
@@ -39,7 +38,13 @@ const nestedMeetings = [
   { startTime: 2, endTime: 5 }
 ];
 
-// console.log(mergeRanges(times1));
+const multipleMerges = [
+  { startTime: 1, endTime: 10 },
+  { startTime: 2, endTime: 6 },
+  { startTime: 3, endTime: 5 },
+  { startTime: 7, endTime: 9 }
+];
+console.log(mergeRanges(times1));
 /*
 [
   { startTime: 0, endTime: 1 },
@@ -50,3 +55,6 @@ const nestedMeetings = [
 
 console.log(mergeRanges(nestedMeetings));
 // [{ startTime: 1, endTime: 8 }];
+
+console.log(mergeRanges(multipleMerges));
+// { startTime: 1, endTime: 10 }
--- interviewCake/arraysAndStrings/mergeMeetingTimes/readme.md
+++ interviewCake/arraysAndStrings/mergeMeetingTimes/readme.md
@@ -55,3 +55,31 @@ so we need to go over that
 cool, there was a problem with my conditional logic. it works now!
 
 ## Brainstorm 2
+
+So I missed an edge case. If one meeting time begins AND ends earlier/later than another, the shorter meeting should just be ignored
+
+Fixed!
+
+## Optimizing
+
+Apparently we can do this in n log n time.
+
+So that's the time of our sort.
+But do my nested for loop take n^2 time?
+
+Yeah, but I probably don't need to inner for loop.
+
+Since the array is sorted, we only need to check the last value in the 'busyTimes' array.
+
+let me modify my function to take out the inner for loop and see if it still works.
+
+_Result_
+So it mostly works, but we have a kind of weird bug.
+
+Cool, so I had to modify my answer to use a c-style for loop that starts at the second element of the input array.
+
+then we preload the 'busyTimes' array with the first value from the sorted input.
+
+as we traverse the rest of the sorted input array, we compare the current object with the LAST object in sortedTimes.
+
+Since the input is sorted, the last time will always be the time we need to compare against.
