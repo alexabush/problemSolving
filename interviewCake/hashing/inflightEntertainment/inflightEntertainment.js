22209d416e2bc4687301d8b840593d4b9cd9f821
Alex Bush <alexabush7@gmail.com>
2018-08-23 15:46:55 -0700

    got log n * n solution by sorted and working towards center of array with high low pointers

--- interviewCake/hashing/inflightEntertainment/inflightEntertainment.js
+++ interviewCake/hashing/inflightEntertainment/inflightEntertainment.js
@@ -1,4 +1,4 @@
-function canTwoMoviesFillFlight(movieLengths, flightLength) {
+function canTwoMoviesFillFlightn2(movieLengths, flightLength) {
   for (let i = 0; i < movieLengths.length; i++) {
     for (let j = 1; j < movieLengths.length; j++) {
       if (movieLengths[i] + movieLengths[j] === flightLength) {
@@ -7,6 +7,22 @@ function canTwoMoviesFillFlight(movieLengths, flightLength) {
     }
   }
 
+  return false;
+}
+
+function canTwoMoviesFillFlight(movieLengths, flightLength) {
+  movieLengths = movieLengths.sort((a, b) => a > b);
+  let low = 0;
+  let high = movieLengths.length - 1;
+  while (low < high) {
+    if (movieLengths[low] + movieLengths[high] === flightLength) {
+      return true;
+    } else if (movieLengths[low] + movieLengths[high] < flightLength) {
+      low++;
+    } else if (movieLengths[low] + movieLengths[high] > flightLength) {
+      high--;
+    }
+  }
   return false;
 }
 
--- interviewCake/hashing/inflightEntertainment/readme.md
+++ interviewCake/hashing/inflightEntertainment/readme.md
@@ -5,3 +5,7 @@
 We want to pick two movies from an array that have a runtime that will add up to a 'flightLength' variable
 
 ## Brainstorm
+
+We can do a nested for loop and compare each pair until one equals flightLength.
+
+Next solution is to sort the array, and then have a high low pointer move in from either side.
