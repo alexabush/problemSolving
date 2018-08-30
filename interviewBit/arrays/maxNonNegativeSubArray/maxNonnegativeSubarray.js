8b7de9871a51d5867c6a3086ff61e1c686d2fa10
Alex Bush <alexabush7@gmail.com>
2018-08-29 17:02:27 -0700

    working on improved version, works for main cases but still fails on some edge cases

--- interviewBit/arrays/maxNonNegativeSubArray/maxNonnegativeSubarray.js
+++ interviewBit/arrays/maxNonNegativeSubArray/maxNonnegativeSubarray.js
@@ -1,4 +1,4 @@
-function maxSet(arr) {
+function maxSetMine(arr) {
   var subArrays = [];
   var subArrayPointer = 0;
   var isNeg = true;
@@ -37,9 +37,47 @@ function maxSet(arr) {
   return subArrays[maxIndex];
 }
 
+function maxSet(arr) {
+  var maxSum = -1,
+    maxStart = -1,
+    maxEnd = -1,
+    currentSum = -1,
+    currentStart = -1,
+    currentEnd = -1;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] >= 0) {
+      currentSum += arr[i];
+      if (currentStart < 0) {
+        currentStart = i;
+      }
+      currentEnd = i;
+      if (currentSum > maxSum) {
+        maxSum = currentSum;
+        maxStart = currentStart;
+        maxEnd = currentEnd;
+      }
+    } else {
+      currentStart = -1;
+      currentEnd = -1;
+      currentSum = 0;
+    }
+  }
+  if (maxEnd > 0) {
+    return arr.slice(maxStart, maxEnd + 1);
+  } else {
+    return [];
+  }
+}
+
 const arr1 = [1, 2, 5, -7, 2, 3];
 const negative1s = [-1, -1, -1, -1, -1];
 const largeNums = [1967513926, 1540383426, -1303455736, -521595368];
+const zeroNeg = [0, 0, -1, 0];
 // console.log(maxSet(arr1));
+// [1,2,5]
 // console.log(maxSet(negative1s));
-console.log(maxSet(largeNums));
+// []
+// console.log(maxSet(largeNums));
+// [1967513926, 1540383426]
+console.log(maxSet(zeroNeg));
+// [0,0]
--- interviewBit/arrays/maxNonNegativeSubArray/readme.md
+++ interviewBit/arrays/maxNonNegativeSubArray/readme.md
@@ -20,3 +20,24 @@ I can map over the subArrays array and sum all the values
 then I can indexOf the highest value, and return the value at that index
 
 not super efficient, but linear time
+
+## Improving my solution
+
+So I have a working solution but it's definately clunky. I have linear time, but I run through the array a bunch, so the total time is probably something like 5n. Which is meh.
+
+_Interview Bit Improvements_
+interview bit suggests not even bothering to make an array, but rather just to keep maxSum, maxStart, and maxEnd pointers and a currentSum, currentStart, and currentEnd pointers as well.
+
+This is a good improvement, so let me try and implement it
+
+## Psuedocode
+
+iterate over array
+test if number is positive
+if it is, we want to add it to the currentSum
+then we want to update the currentStart/currentEnd
+then we want to check if we should replace the maxSum with the currentSum
+
+## Debugging 2
+
+need to modify so 0 counts as a positive number
