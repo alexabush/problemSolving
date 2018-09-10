855081355101e99ed7769329bb2563cb62ae5a00
Alex Bush <alexabush7@gmail.com>
2018-09-10 13:29:18 -0700

    solved problem, linear time

---
+++ codeRust/arrays/rotateArray/readme.md
@@ -0,0 +1,14 @@
+# Rotate Array
+
+# What is the problem?
+
+given an array of integers, rotate the array by N elements
+
+## Brainstorm
+
+should be done in linear time
+
+we can just unshift the first n elements and push them to the end?
+
+pop/unshift for negative numbers
+shift/push for positive numbers
---
+++ codeRust/arrays/rotateArray/rotateArray.js
@@ -0,0 +1,24 @@
+function rotateArray(arr, n) {
+  if (n < 0) {
+    for (let i = 0; i < Math.abs(n); i++) {
+      let num = arr.shift();
+      arr.push(num);
+    }
+  } else {
+    for (let i = 0; i < n; i++) {
+      let num = arr.pop();
+      arr.unshift(num);
+    }
+  }
+  return arr;
+}
+
+// const arr1 = [1, 10, 20, 0, 59, 86, 32, 11, 9, 40];
+const a2 = [1, 2, 3, 4, 5];
+// console.log(rotateArray(arr1, -1));
+// console.log(rotateArray(a2, 1));
+// [5,1,2,3,4]
+console.log(rotateArray(a2, -1));
+// [2,3,4,5,1]
+// console.log(rotateArray(a2, 2));
+// [4,5,1,2,3]
