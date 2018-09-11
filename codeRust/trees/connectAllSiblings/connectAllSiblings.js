19d364bb4846e0b7a33f4fe905338a3facd62150
Alex Bush <alexabush7@gmail.com>
2018-09-11 09:31:11 -0700

    finsihed connectAllSiblings

--- codeRust/trees/connectAllSiblings/connectAllSiblings.js
+++ codeRust/trees/connectAllSiblings/connectAllSiblings.js
@@ -10,4 +10,24 @@ function connectAllSiblings(root) {
       queue.push(node.right);
     }
   }
+  return root;
 }
+
+class TreeNode {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+    this.next = null;
+  }
+}
+
+let tree1 = new TreeNode(100);
+tree1.left = new TreeNode(50);
+tree1.left.right = new TreeNode(75);
+tree1.left.left = new TreeNode(25);
+tree1.right = new TreeNode(200);
+tree1.right.right = new TreeNode(300);
+tree1.right.right.right = new TreeNode(350);
+
+console.log(connectAllSiblings(tree1));
