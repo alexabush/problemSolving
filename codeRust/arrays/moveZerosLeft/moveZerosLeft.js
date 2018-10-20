055b2d0d9966c564115b67022269b253a37e3246
Alex Bush <alexabush7@gmail.com>
2018-10-20 16:43:27 -0700

    trying to figure out a linear time solution, still working on it

--- codeRust/arrays/moveZerosLeft/moveZerosLeft.js
+++ codeRust/arrays/moveZerosLeft/moveZerosLeft.js
@@ -12,6 +12,20 @@ function moveZerosLeftPure(nums) {
 
 function moveZerosLeft(nums) {
   let zeroCount = 0;
+  for (let num of nums) {
+    if (num === 0) zeroCount++;
+  }
+  let newArr = new Array(zeroCount).fill(0);
+  for (let num of nums) {
+    if (num !== 0) newArr.push(num);
+  }
+  debugger;
+  nums = newArr;
+  return nums;
+}
+
+function moveZerosLeftInPlacen2(nums) {
+  let zeroCount = 0;
   nums.forEach((num, index, nums) => {
     if (num === 0) {
       nums.splice(index, 1);
@@ -24,4 +38,6 @@ function moveZerosLeft(nums) {
 }
 
 let arr1 = [1, 10, 20, 0, 59, 63, 0, 88, 0];
-console.log(moveZerosLeft(arr1));
+debugger;
+moveZerosLeft(arr1);
+console.log(arr1);
