45c981cf6dbf0153df262333eef66f68a39778cf
Alex Bush <alexabush7@gmail.com>
2018-10-20 16:26:35 -0700

    got working in place solution

--- codeRust/arrays/moveZerosLeft/moveZerosLeft.js
+++ codeRust/arrays/moveZerosLeft/moveZerosLeft.js
@@ -1,14 +1,26 @@
-function moveZerosLeft(nums) {
+function moveZerosLeftPure(nums) {
   let zeroCount = 0;
   for (let num of nums) {
     if (num === 0) zeroCount++;
   }
   let newArr = new Array(zeroCount).fill(0);
-  debugger;
   for (let num of nums) {
     if (num !== 0) newArr.push(num);
   }
   return newArr;
+}
+
+function moveZerosLeft(nums) {
+  let zeroCount = 0;
+  nums.forEach((num, index, nums) => {
+    if (num === 0) {
+      nums.splice(index, 1);
+      zeroCount++;
+    }
+  });
+  let zeros = new Array(zeroCount).fill(0);
+  nums.unshift(...zeros);
+  return nums;
 }
 
 let arr1 = [1, 10, 20, 0, 59, 63, 0, 88, 0];
