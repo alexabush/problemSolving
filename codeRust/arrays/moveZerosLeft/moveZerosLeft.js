48abe06461eda3d34fc70238efdc60ab853b8f51
Alex Bush <alexabush7@gmail.com>
2018-10-22 18:34:59 -0700

    made change

--- codeRust/arrays/moveZerosLeft/moveZerosLeft.js
+++ codeRust/arrays/moveZerosLeft/moveZerosLeft.js
@@ -10,21 +10,21 @@ function moveZerosLeftPure(nums) {
   return newArr;
 }
 
-function moveZerosLeft(nums) {
-  let zeroCount = 0;
-  for (let num of nums) {
-    if (num === 0) zeroCount++;
-  }
-  let newArr = new Array(zeroCount).fill(0);
-  for (let num of nums) {
-    if (num !== 0) newArr.push(num);
-  }
-  debugger;
-  nums = newArr;
-  return nums;
-}
+// function moveZerosLeft(nums) {
+//   let zeroCount = 0;
+//   for (let num of nums) {
+//     if (num === 0) zeroCount++;
+//   }
+//   let newArr = new Array(zeroCount).fill(0);
+//   for (let num of nums) {
+//     if (num !== 0) newArr.push(num);
+//   }
+//   debugger;
+//   nums = newArr;
+//   return nums;
+// }
 
-function moveZerosLeftInPlacen2(nums) {
+function moveZerosLeftn2(nums) {
   let zeroCount = 0;
   nums.forEach((num, index, nums) => {
     if (num === 0) {
@@ -37,7 +37,32 @@ function moveZerosLeftInPlacen2(nums) {
   return nums;
 }
 
-let arr1 = [1, 10, 20, 0, 59, 63, 0, 88, 0];
-debugger;
-moveZerosLeft(arr1);
-console.log(arr1);
+// reverse iteration + swap window strategy
+function moveZerosLeft(nums) {
+  let swapWindow = 0;
+  //we might need to modify this when we get near the beginning of the list?
+  for (let i = nums.length - 1; i > 0; i--) {
+    if (nums[i - swapWindow] === 0) {
+      swapWindow++;
+    }
+    debugger;
+    if (swapWindow > 0) {
+      swap(nums, i - swapWindow, i);
+    }
+  }
+  return nums;
+
+  // I think this constant even though it uses slice, because our swapWindow is constant
+  function swap(arr, start, end) {
+    let temp = arr[start];
+    arr[start] = arr[end];
+    arr[end] = temp;
+  }
+}
+
+let arr1 = [1, 10, 20, 0];
+let arr2 = [1, 10, 20, 0, 59, 63, 0];
+let arr3 = [1, 10, 20, 0, 59, 63, 0, 88, 0];
+
+moveZerosLeft(arr2);
+console.log(arr2);
