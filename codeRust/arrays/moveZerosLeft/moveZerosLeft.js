3d16f8207c9c9d2baa93e5d83dfae68e421cd2f0
Alex Bush <alexabush7@gmail.com>
2018-10-22 18:50:48 -0700

    made change

--- codeRust/arrays/moveZerosLeft/moveZerosLeft.js
+++ codeRust/arrays/moveZerosLeft/moveZerosLeft.js
@@ -38,14 +38,13 @@ function moveZerosLeftn2(nums) {
 }
 
 // reverse iteration + swap window strategy
-function moveZerosLeft(nums) {
+function moveZerosLeftNonWorking(nums) {
   let swapWindow = 0;
   //we might need to modify this when we get near the beginning of the list?
   for (let i = nums.length - 1; i > 0; i--) {
     if (nums[i - swapWindow] === 0) {
       swapWindow++;
     }
-    debugger;
     if (swapWindow > 0) {
       swap(nums, i - swapWindow, i);
     }
@@ -59,10 +58,30 @@ function moveZerosLeft(nums) {
     arr[end] = temp;
   }
 }
+// CodeRust answer
+function moveZerosLeft(nums) {
+  if (nums.length < 1) {
+    return;
+  }
+  let lengthA = nums.length;
+  let write_index = lengthA - 1;
+  let read_index = lengthA - 1;
+  while (read_index >= 0) {
+    if (nums[read_index] != 0) {
+      nums[write_index] = nums[read_index];
+      write_index--;
+    }
+    read_index--;
+  }
+  while (write_index >= 0) {
+    nums[write_index] = 0;
+    write_index--;
+  }
+}
 
 let arr1 = [1, 10, 20, 0];
 let arr2 = [1, 10, 20, 0, 59, 63, 0];
 let arr3 = [1, 10, 20, 0, 59, 63, 0, 88, 0];
 
-moveZerosLeft(arr2);
-console.log(arr2);
+moveZerosLeft(arr3);
+console.log(arr3);
