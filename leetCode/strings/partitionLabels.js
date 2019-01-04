0e2be950118a40a0e00f7c2937e5de99f24a3af7
Alex Bush <alexabush7@gmail.com>
2019-01-03 17:04:27 -0800

    partition labels

--- leetCode/strings/partitionLabels.js
+++ leetCode/strings/partitionLabels.js
@@ -3,17 +3,11 @@ function partitionLabels(str) {
   for (let i = 0; i < str.length; i++) {
     let letter = str[i];
     if (!letters[letter]) letters[letter] = [i];
-    else letters[letter].push(i);
-  }
-  let ranges = Object.entries(letters).map((data, i, obj) => {
-    let letter = data[0],
-      indexes = data[1];
-    if (indexes.length > 2) {
-      return [indexes[0], indexes[indexes.length - 1]];
+    else {
+      letters[letter][1] = i;
     }
-    return indexes;
-  });
-  let sortedRanges = ranges.sort((a, b) => {
+  }
+  let sortedRanges = Object.values(letters).sort((a, b) => {
     if (a[0] <= b[0]) {
       return -1;
     }
@@ -26,16 +20,11 @@ function partitionLabels(str) {
       continue
     }
     let current = partitions[partitions.length - 1];
-    let start1 = current[0],
-      end1 = current[1] || current[0];
+    let end1 = current[1] || current[0];
     let start2 = range[0],
       end2 = range[1] || range[0];
-    if (start1 < start2 && end1 > end2) continue
-    else if (start2 < start1 && end2 > end1) {
-      partitions[partitions.length - 1] = range
-    } else if (start1 < start2 && start2 > end1) {
-      partitions.push(range)
-    } else if (start1 < start2 && end2 > end1) {
+    if (start2 > end1) partitions.push(range)
+    else if (end2 > end1) {
       partitions[partitions.length - 1][1] = end2
     }
   }
@@ -47,6 +36,7 @@ function partitionLabels(str) {
   })
 }
 
-let str1 = 'firelight is amazing'; // 2
+let str1 = 'firelight is amazing'; // [1,19]
 let str2 = "ababcbacadefegdehijhklij" // [9, 7, 8]
+console.log(partitionLabels(str1))
 console.log(partitionLabels(str2))
