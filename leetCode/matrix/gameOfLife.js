c8ab0911cdf11d17e9c2291eae0a9285a917cb9f
Alex Bush <alexabush7@gmail.com>
2019-01-07 09:53:59 -0800

    finished gameOfLife, linear space, linear time where n is the total number of cells in the board (in big O, actual time more like n * 8 + n since I go through the full board twice)

---
+++ leetCode/matrix/gameOfLife.js
@@ -0,0 +1,56 @@
+function gameOfLife(board) {
+  let newBoard = board.map(row => row.map(cell => cell));
+  for (let i = 0; i < board.length; i++) {
+    let row = board[i];
+    for (let j = 0; j < board[0].length; j++) {
+      let cell = board[i][j];
+      let neighbors = 0;
+      if (i > 0 && board[i - 1][j] === 1) {
+        neighbors++;
+      } //up
+      if (i < board.length - 1 && board[i + 1][j] === 1) {
+        neighbors++;
+      } //down
+      if (j > 0 && board[i][j - 1] === 1) {
+        neighbors++;
+      } // left
+      if (j < board[0].length - 1 && board[i][j + 1] === 1) {
+        neighbors++;
+      } //right
+
+      if (i > 0 && j > 0 && board[i - 1][j - 1] === 1) {
+        neighbors++;
+      } //up-left
+      if (i < board.length - 1 && j > 0 && board[i + 1][j - 1] === 1) {
+        neighbors++;
+      } //down-left
+      if (i > 0 && j < board[i].length - 1 && board[i - 1][j + 1] === 1) {
+        neighbors++;
+      } // up-right
+      if (
+        i < board.length - 1 &&
+        j < board[0].length - 1 &&
+        board[i + 1][j + 1] === 1
+      ) {
+        neighbors++;
+      } //down-right
+      if (board[i][j] == 1 && neighbors < 2) newBoard[i][j] = 0;
+      if (board[i][j] == 1 && neighbors > 3) newBoard[i][j] = 0;
+      if (board[i][j] == 0 && neighbors === 3) newBoard[i][j] = 1;
+    }
+  }
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[0].length; j++) {
+      board[i][j] = newBoard[i][j]
+    }
+  }
+}
+
+// let board = [[0, 1, 0], [0, 0, 1], [1, 1, 1], [0, 0, 0]];
+// gameOfLife(board);
+// console.log(board);
+
+let tinyBoard = [[1,1],[1,0]];
+gameOfLife(tinyBoard);
+console.log(tinyBoard);
+
