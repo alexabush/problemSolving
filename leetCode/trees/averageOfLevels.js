43bb15963eefe666784823fb6659854807702652
Alex Bush <alexabush7@gmail.com>
2019-01-09 09:23:12 -0800

    added bfs solution based on solution from leetcode

--- leetCode/trees/averageOfLevels.js
+++ leetCode/trees/averageOfLevels.js
@@ -1,13 +1,33 @@
-var averageOfLevels = function (root) {
-  let averages = []
-  helper(root)
-  return averages.map(levelAv => levelAv[0] / levelAv[1])
+// dfs solution, decent runtime (linear time, linear space)
+var averageOfLevels = function(root) {
+  let averages = [];
+  helper(root);
+  return averages.map(levelAv => levelAv[0] / levelAv[1]);
 
   function helper(node, level = 1) {
-    if (typeof averages[level - 1] === 'undefined') averages[level - 1] = [0, 0]
-    averages[level - 1][0] += node.val
-    averages[level - 1][1] += 1
-    if (node.left) helper(node.left, level + 1)
-    if (node.right) helper(node.right, level + 1)
+    if (typeof averages[level - 1] === 'undefined')
+      averages[level - 1] = [0, 0];
+    averages[level - 1][0] += node.val;
+    averages[level - 1][1] += 1;
+    if (node.left) helper(node.left, level + 1);
+    if (node.right) helper(node.right, level + 1);
   }
 };
+
+// bfs solution, based on answer from Leetcode
+function averageLength(root) {
+  let results = [];
+  let queue = [root];
+  while (queue.length) {
+    let levelLength = queue.length;
+    let levelSum = 0;
+    for (let i = 0; i < levelLength; i++) {
+      let node = queue.shift();
+      levelSum += node.val;
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+    results.push(levelSum / levelLength);
+  }
+  return results;
+}
