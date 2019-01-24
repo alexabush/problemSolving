3f019a181050b4fd38072190ea147d652e80eee7
Alex Bush <alexabush7@gmail.com>
2019-01-24 10:58:51 -0800

    finished validSudoku after looking at answer on leetcode. Set up unit tests

--- leetCode/matrix/validSudoku.js
+++ leetCode/matrix/validSudoku.js
@@ -2,81 +2,62 @@
  * @param {character[][]} board
  * @return {boolean}
  */
-var isValidSudoku = function (board) {
-  // vertical
-  // for (let i = 0; i < board.length; i++) {
-  //   if (!isValid(board[i])) {
-  //     return false
-  //   }
-  // }
-  // horizontal
-  // let cols = []
-  // for (let i = 0; i < board.length; i++) {
-  //   for (let j = 0; j < board.length; j++) {
-  //     if (!cols[j]) cols[j] = []
-  //     cols[j].push(board[i][j])
-  //   }
-  // }
-  // for (let col of cols) {
-  //   if (!isValid(col)) return false
-  // }
-  // sub-boxes
-  let boxes = []
-  for (let i = 0; i < board.length; i += 3) {
-    debugger
-    let boxNum = Math.floor(i / 3)
-    let box = []
-    boxes[boxNum] = box
-    for (let j = i; j < i+3; j+=3) {
-      debugger
-      for (let k = j; k < j+3; k += 1) {
-        debugger
-        box.push(board[j][k])
-        box.push(board[j+1][k])
-        box.push(board[j+2][k])
-      }
-    }
+var isValidSudoku = function(board) {
+  if (checkVertical(board) && checkHorizontal(board) && checkSquares(board)) {
+    return true;
+  } else {
+    return false;
   }
-  // console.log(boxes)
-  return true
 
-  function isValid(numGroup) {
-    let seen = {}
-    for (let num of numGroup) {
-      if (num === '.') continue
-      if (seen[num]) {
-        return false
+  function checkVertical(board) {
+    for (let i = 0; i < board.length; i++) {
+      if (!isValid(board[i])) {
+        return false;
       }
-      seen[num] = 1
     }
     return true
   }
+
+  function checkHorizontal(board) {
+    let cols = [];
+    for (let i = 0; i < board.length; i++) {
+      for (let j = 0; j < board.length; j++) {
+        if (!cols[j]) cols[j] = [];
+        cols[j].push(board[i][j]);
+      }
+    }
+    for (let col of cols) {
+      if (!isValid(col)) return false;
+    }
+    return true
+  }
+
+  function checkSquares(board) {
+    for (let i = 0; i < board.length; i += 3) {
+      for (let j = 0; j < board[0].length; j += 3) {
+        let square = [];
+        for (let k = i; k < i + 3; k++) {
+          for (let l = j; l < j + 3; l++) {
+            square.push(board[k][l]);
+          }
+        }
+        if (!isValid(square)) return false;
+      }
+    }
+    return true
+  }
+
+  function isValid(numGroup) {
+    let seen = {};
+    for (let num of numGroup) {
+      if (num === '.') continue;
+      if (seen[num]) {
+        return false;
+      }
+      seen[num] = 1;
+    }
+    return true;
+  }
 };
 
-// let board1 =
-//   [
-//     [".", ".", ".", ".", "5", ".", ".", "1", "."],
-//     [".", "4", ".", "3", ".", ".", ".", ".", "."],
-//     [".", ".", ".", ".", ".", "3", ".", ".", "1"],
-//     ["8", ".", ".", ".", ".", ".", ".", "2", "."],
-//     [".", ".", "2", ".", "7", ".", ".", ".", "."],
-//     [".", "1", "5", ".", ".", ".", ".", ".", "."],
-//     [".", ".", ".", ".", ".", "2", ".", ".", "."],
-//     [".", "2", ".", "9", ".", ".", ".", ".", "."],
-//     [".", ".", "4", ".", ".", ".", ".", ".", "."]
-//   ]
-
-
-let board1 = [
-  ["8", "3", ".", ".", "7", ".", ".", ".", "."],
-  ["6", ".", ".", "1", "9", "5", ".", ".", "."],
-  [".", "9", "8", ".", ".", ".", ".", "6", "."],
-  ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
-  ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
-  ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
-  [".", "6", ".", ".", ".", ".", "2", "8", "."],
-  [".", ".", ".", "4", "1", "9", ".", ".", "5"],
-  [".", ".", ".", ".", "8", ".", ".", "7", "9"]
-]
-
-console.log(isValidSudoku(board1));
+module.exports = { isValidSudoku };
